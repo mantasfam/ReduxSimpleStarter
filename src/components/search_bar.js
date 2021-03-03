89ae3b325af3cc4156d96ca67154fe6d6cfe13b2
Mantas Stašauskas <mantas487@gmail.com>
2021-03-03 18:55:21 +0200

    class vs function

---
+++ src/components/search_bar.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component{
+  render() { //butinas render and return jsx klasei
+    return <input />;
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCpnK1GGLSVVvFJ-u2BOuUe0ZJwZ28hfsc';
+
 // Create a new component.
 // This component should produce some html
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
+
+
 
 // Take this component's generated HTML and put
 // on the page (in the ?DOM?)
