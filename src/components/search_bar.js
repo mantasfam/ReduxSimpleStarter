9a946e44f59a8faead55592b77ef307dc37c4b56
Mantas Stašauskas <mantas487@gmail.com>
2021-03-03 20:05:36 +0200

    onChange

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component{
   render() { //butinas render and return jsx klasei
-    return <input />;
+    return <input onChange={(event) => console.log(event.target.value)}/>;
   }
 }
-
 export default SearchBar;
