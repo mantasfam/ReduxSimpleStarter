c4b456a49eb94aec55915ba7e0ca72326b3effac
Mantas Stašauskas <mantas487@gmail.com>
2021-03-04 13:10:33 +0200

    App from function to class

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCpnK1GGLSVVvFJ-u2BOuUe0ZJwZ28hfsc';
 
+
+
 // Create a new component.
 // This component should produce some html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos_array) => {
+      this.setState({ videos: videos_array}); // trumpiau: videos: videos ----> videos (kai pavinimai sutmapa)
+    })
+  }
+
+  render() { //butinas render and return jsx klasei
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
+}
 
 
 // Take this component's generated HTML and put
