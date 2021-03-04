d06e7bcf88796cb952d4d16e73ebe316c48e336e
Mantas Stašauskas <mantas487@gmail.com>
2021-03-04 14:39:28 +0200

    array

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCpnK1GGLSVVvFJ-u2BOuUe0ZJwZ28hfsc';
 
@@ -16,7 +17,7 @@ class App extends Component {
 
     this.state = { videos: [] };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos_array) => {
+    YTSearch({key: API_KEY, term: 'chess'}, (videos_array) => {
       this.setState({ videos: videos_array}); // trumpiau: videos: videos ----> videos (kai pavinimai sutmapa)
     })
   }
@@ -25,6 +26,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     )
   }
